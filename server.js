81a1af296f27ae6305cd6a875ca0403489660016
merwhite11 <meredith.white91@gmail.com>
2022-03-22 12:23:08 -0700

    Uncommented redis

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 // app.use('/ratings', createProxyMiddleware({target: 'http://localhost:5000'}))
 
-const redis = require('redis')
-const redisPort = 6379
-const client = redis.createClient({
-  host: '54.147.168.66',
-  port: redisPort,
-  legacyMode: true
-})
-client.connect()
-client.on('error', (err) => {
-  console.log(err)
-})
+// const redis = require('redis')
+// const redisPort = 6379
+// const client = redis.createClient({
+//   host: '54.147.168.66',
+//   port: redisPort,
+//   legacyMode: true
+// })
+// client.connect()
+// client.on('error', (err) => {
+//   console.log(err)
+// })
 
 const model = require('./index.js')
 const port = 5000
@@ -28,17 +28,17 @@ app.get('/loaderio-d31975cfff2e598ccb1a6a1760dc0426.txt', (req, res) => {
 
 app.get('/ratings/reviews/:productId', (req, res) => {
   const {productId} = req.params
-  client.get(productId, async (err, reviews) => {
-    if (err) throw err;
-    if (reviews) {
-      res.status(200).send({
-        reviews: JSON.parse(reviews),
-        message: "data retrieved from cache"
-      })
-    } else {
+  // client.get(productId, async (err, reviews) => {
+    // if (err) throw err;
+    // if (reviews) {
+    //   res.status(200).send({
+    //     reviews: JSON.parse(reviews),
+    //     message: "data retrieved from cache"
+    //   })
+    // } else {
       getReviews(req.params.productId)
       .then((response) => {
-        client.setex(productId, 600, JSON.stringify(response));
+        // client.setex(productId, 600, JSON.stringify(response));
         // console.log('ratings response in server', response)
         res.send(response).status(200)
       })
@@ -46,9 +46,9 @@ app.get('/ratings/reviews/:productId', (req, res) => {
         console.log('err in app.get /ratings', err.message)
         res.sendStatus(500)
       })
-    }
-  })
-})
+    })
+  // }
+// })
 
 app.get('/ratings/characteristics/:productId', (req, res) => {
   getCharacteristicReviews(req.params.productId)
